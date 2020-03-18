590ae4d5ed613581878e868f80fb4f65a8c555bf
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-18 19:39:05 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,16 +9,48 @@ def directors_totals(nds)
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   totals = {}
+  dir_counter = 0;
 
+  while dir_counter < nds.size do
+    name = nds[dir_counter][:name]
+    gross_total = 0
 
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
+    movie_counter = 0
 
+      while movie_counter < nds[dir_counter][:movies].size do
+            gross = nds[dir_counter][:movies][movie_counter][:worldwide_gross]
+            totals[name] += gross
+          movie_counter += 1
+      end
+      dir_counter += 1
+  end
+
+  totals
 end
+
+[{:name=>"Stephen Spielberg",
+  :movies=>
+   [{:title=>"Jaws",
+     :studio=>"Universal",
+     :worldwide_gross=>260000000,
+     :release_year=>1975},
+    {:title=>"Close Encounters of the Third Kind",
+     :studio=>"Columbia",
+     :worldwide_gross=>135189114,
+     :release_year=>1977},
+    {:title=>"Raiders of the Lost Ark",
+     :studio=>"Paramount",
+     :worldwide_gross=>248159971,
+     :release_year=>1981},
+    {:title=>"E.T. the Extra-terrestrial",
+     :studio=>"Universal",
+     :worldwide_gross=>435110554,
+     :release_year=>1982},
+    {:title=>"Schindler's List",
+     :studio=>"Universal",
+     :worldwide_gross=>96898818,
+     :release_year=>1993},
+    {:title=>"Lincoln",
+     :studio=>"Buena Vista",
+     :worldwide_gross=>182207973,
+     :release_year=>2012}]},
